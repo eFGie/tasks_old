739c50acb8673b1c770d045289c225219509ed33
eFGie <filip.grodzki@gmail.com>
2019-08-18 16:50:22 +0200

    Commit

--- Coding/Projects/tasks/src/main/java/com/crud/tasks/domain/TaskDao.java
+++ Coding/Projects/tasks/src/main/java/com/crud/tasks/domain/TaskDao.java
@@ -1,8 +1,10 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class TaskDao {
     private Long id;
     private String title;
