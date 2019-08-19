f247469d9d1626f5455ba2dcd5dd7b9ab24d6545
eFGie <filip.grodzki@gmail.com>
2019-08-19 22:31:10 +0200

    Commit

--- Coding/Projects/tasks/src/main/java/com/crud/tasks/controller/TaskController.java
+++ Coding/Projects/tasks/src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,15 +1,17 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/task")
@@ -19,15 +21,14 @@ public class TaskController {
     @Autowired
     private TaskMapper taskMapper;
 
-
     @RequestMapping(method = RequestMethod.GET,value = "getTasks")
     public List<TaskDto> getTasks() {
         return taskMapper.mapToTaskDtoList(service.getAllTasks());
-   }
+    }
 
-    @RequestMapping(method = RequestMethod.GET,value = "getTask")
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L,"test title","test_content");
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
+    public TaskDto getTask(@RequestParam("ID") Long taskId) {
+        return taskMapper.mapToTaskDto(service.getTaskById(taskId));
     }
 
     @RequestMapping(method = RequestMethod.DELETE,value = "deleteTask")
--- Coding/Projects/tasks/src/main/java/com/crud/tasks/service/DbService.java
+++ Coding/Projects/tasks/src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -15,4 +16,12 @@ public class DbService {
     public List<Task> getAllTasks() {
         return repository.findAll();
     }
+
+    public Task getTaskById(Long i) {
+        Optional<Task> optionalTask =  repository.findById(i);
+        Task task = optionalTask.get();
+        return task;
+    }
+
+
 }
